b952b4792e1dfef954ff1d5c6a70a60968cd9b33
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-12 22:47:20 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Kusunose Hosho on 2020/05/12.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
